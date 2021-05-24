611d1212fc156f5d7fcd985c50d423a8ea767bc3
junior <jiraphat-saeheng@hotmail.com>
2021-05-24 18:16:48 +0700

    eeeee

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'listitem.dart';
 
@@ -14,15 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'The vote',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+      
         primarySwatch: Colors.blue,
       ),
       home: VoteList(),
