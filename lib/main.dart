dc81de1c41236a109ff987c940b23ef35f58645e
junior <jiraphat-saeheng@hotmail.com>
2021-05-28 16:04:01 +0700

    'surprise'

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
         },
         debugShowCheckedModeBanner: false,
         title: 'The vote',
-        theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Heebo'),
+        theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'VarelaRound'),
         home: Mainpags(),
       ),
     );
